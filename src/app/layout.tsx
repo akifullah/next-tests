948d046277f8924187d17de93b0384e417f8b7d2
Akif Ullah <akifullah0317@gmail.com>
2025-08-12 17:10:04 +0500

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         cz-shortcut-listen="true"
       >
-        <ReduxProvider initialState={{}}>
+        <ReduxProvider initialState>
           {children}
         </ReduxProvider>
       </body>
